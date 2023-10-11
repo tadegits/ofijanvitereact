cec279d36f0b98b921d2ade445309d797257c431
millionsime <simemillion@gmail.com>
2023-10-11 16:00:05 +0300

    insane nav

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,41 +4,48 @@ import Logo from "../../assets/logo.png";
 import {FaBars} from "react-icons/fa";
 import {useState} from "react";
 const Navbar= () => {
-  const[showNav, setShowNav] =useState(false);
+  const[showNav, setShowNav] =useState
+  (false);
 
-    return (
+     return (
     
-    <nav>
+    <nav className="navbar">
         
-        <Wrapper className="navbar_container">
-     <a href = "#" className="navbar_logo"
+        <Wrapper className="navbar__container">
+     <a href = "#" className="navbar__logo"
      onClick={() => setShowNav(false)}>
-        <img src={Logo}alt=""/>
+        <img src={Logo} alt=""/>
      </a>
      <ul className={'navbar__links ${showNav ? "show-nav" :"" }'}>
         <li onClick={() => setShowNav(flase)}>
             <a href ="#">Home</a>
         </li>
         <li onClick={() => setShowNav(flase)}>
-            <a href ="#">About</a>
+            <a href ="#">Grade 6th</a>
         </li>
         <li onClick={() => setShowNav(flase)}>
-            <a href ="#"></a>
+            <a href ="#">Grade 8th</a>
         </li>
         <li onClick={() => setShowNav(flase)}>
-            <a href ="#">Home</a>
+            <a href ="#">Grade 12</a>
         </li>
         <li onClick={() => setShowNav(flase)}>
-            <a href ="#">Home</a>
+            <a href ="#">Exit Exam</a>
+        </li>
+        <li onClick={() => setShowNav(flase)}>
+            <a href ="#">about</a>
         </li>
 </ul>
 
-<a href="#" className="button-primary navbar__btn">
+<a href="#" className="button-primary 
+navbar__btn">
     Sign Up
 </a>
 
-<div className={'navbar__menubar ${showNav ? "bg-color": "" }'} 
-onClick={()=>setShowNav(!showNav)}>
+<div className={'navbar__menubar ${showNav ? "bg-color": ""}'} 
+onClick={()=>setShowNav(!showNav)}
+>
+    <FaBars />
      </div>
     </Wrapper>
     </nav>
