8830fdff3c9b8a4602355c4653053ce91af6dda0
millionsime <simemillion@gmail.com>
2023-10-12 13:34:19 +0300

    signup done

--- src/components/SignUpSection/SignUpSection.jsx
+++ src/components/SignUpSection/SignUpSection.jsx
@@ -1,20 +1,21 @@
 import "./SignUpSection.scss";
 import Wrapper from "../wrapper/Wrapper";
 import React from 'react';
-import Phone from '../../assets/sun3.png'
+import SignUp from '../../assets/signup.png'
 
 export default function SignUpSection() {
   return (
     <section className="signup">
 <Wrapper className="signup__container">
 <div className="signup_left">
-     <img src={Phone}></img> 
+     <img src={SignUp}></img> 
 </div>
 <div className="signup_right">
-    <h3>Join to test yourself!</h3>
+    <h3>Join and test what you have studied so far!</h3>
     <p>
         {" "}
-        Simple registration will customise your preference! You will have better experinace with ofijan.
+        Simple registration will customise your preference! You will have better experinace with ofijan. 
+        we are waiting for you with all our best questions to practise with.
     </p>
     <form>
         <input 
