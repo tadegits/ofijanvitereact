8aa81a13f34ec4a4b1dd356bcb3ebd295b3825f4
millionsime <simemillion@gmail.com>
2023-10-12 12:16:09 +0300

    signup

--- src/components/SignUpSection/SignUpSection.jsx
+++ src/components/SignUpSection/SignUpSection.jsx
@@ -1,30 +1,33 @@
 import "./SignUpSection.scss";
 import Wrapper from "../wrapper/Wrapper";
-const SignUpSection = () =>{
-    return (
-        <section className="signup">
+import React from 'react';
+import Phone from '../../assets/sun3.png'
+
+export default function SignUpSection() {
+  return (
+    <section className="signup">
 <Wrapper className="signup__container">
-    <div className="signup_left">
-        {/* <img></img> */}
-    </div>
-    <div className="signup_right">
-        <h3>Join to test yourself!</h3>
-        <p>
-            {" "}
-            Simple registration will customise your preference! You will have better experinace with ofijan.
-        </p>
-        <form>
-            <input 
-            type="email"
-            placeholder="Enter your Email"
-            className="Email"/>
-            <input type="submit"
-            value="submit"
-            className="button-primary"/>
-        </form>
-    </div>
+<div className="signup_left">
+     <img src={Phone}></img> 
+</div>
+<div className="signup_right">
+    <h3>Join to test yourself!</h3>
+    <p>
+        {" "}
+        Simple registration will customise your preference! You will have better experinace with ofijan.
+    </p>
+    <form>
+        <input 
+        type="email"
+        placeholder="Enter your Email"
+        className="Email"/>
+        <input type="submit"
+        value="submit"
+        className="button-primary"/>
+    </form>
+</div>
 </Wrapper>
-        </section>
-        
-    )
+    </section>
+    
+)
 }
