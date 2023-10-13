a9fe9097f5f92b44e3807114cb31685cc2ad8094
millionsime <simemillion@gmail.com>
2023-10-13 11:18:02 +0300

    abdu joined

--- src/components/Grade6/Grade6.jsx
+++ src/components/Grade6/Grade6.jsx
@@ -1,83 +1,34 @@
-import '../featuresSection/FeaturesSection.scss'
+import './Grade6.scss'
 import Wrapper from '../wrapper/Wrapper'
+import Questions from '../Questions/Questions'
 import Img1 from '../../assets/resource1.png'
 import Img2 from '../../assets/resource2.png'
 import Img3 from '../../assets/resource3.png'
 import Img4 from '../../assets/resource4.png'
 import Img5 from '../../assets/resource5.png'
 import Img6 from '../../assets/resource6.png'
-
+import { Link } from "react-router-dom";
+import { useRef } from 'react'
 const Grade8 = () => {
+  const course_name = useRef(0);
   return <section className="features">
     <Wrapper>
-      <div className="features__text">
-        <h3>6</h3>
-        <p>
-          Lose john poor same it case do year we Full how way even sigh Extremely nor furniture fat questions now provision incommode.
-        </p>
+      <div courses_container>
+      <Link to="#" className="button-courses"> English</Link>
+      <a href="#" className="button-courses">Mathematics</a>
+      <a href="#" className="button-courses">Afaan Oromo</a>
+      <a href="#" className="button-courses">Sience</a>
+      <a href="#" className="button-courses"> English</a>
+      <a href="#" className="button-courses">Mathematics</a>
+        <h5 ref={course_name}> Here are sample English exam wuastions from our database. click See more to get all</h5> 
+        
       </div>
-      <div className="features__container">
-        <div className="features__feature dark-feature">
-          <img src={Img1} width={200} alt="" />
-          <h4 className="dark-feature-title">
-            Free Enrollment
-          </h4>
-          <p>
-            Engaged was the evident pleased husband. Ye goodness falicity does disposal dwellin no.
-          </p>
-        </div>
-
-        <div className="features__feature white-feature">
-          <img src={Img2} width={200} alt="" />
-          <h4 className="white-feature-title">
-            Fast and easy registration
-          </h4>
-          <p>
-            Engaged was the evident pleased husband. Ye goodness falicity does disposal dwellin no.
-          </p>
-        </div>
-
-        <div className="features__feature dark-feature">
-          <img src={Img3} width={200} alt="" />
-          <h4 className="dark-feature-title">
-            Crypto Support
-          </h4>
-          <p>
-            Engaged was the evident pleased husband. Ye goodness falicity does disposal dwellin no.
-          </p>
-        </div>
-
-        <div className="features__feature white-feature">
-          <img src={Img4} width={200} alt="" />
-          <h4 className="white-feature-title">
-            No Hidden Fees
-          </h4>
-          <p>
-            Engaged was the evident pleased husband. Ye goodness falicity does disposal dwellin no.
-          </p>
-        </div>
-
-        <div className="features__feature dark-feature">
-          <img src={Img5} width={200} alt="" />
-          <h4 className="dark-feature-title">
-            Automatic card lock/unlock
-          </h4>
-          <p>
-            Engaged was the evident pleased husband. Ye goodness falicity does disposal dwellin no.
-          </p>
-        </div>
-
-        <div className="features__feature white-feature">
-          <img src={Img6} width={200} alt="" />
-          <h4 className="white-feature-title">
-            24/7 Customer Support
-          </h4>
-          <p>
-            Engaged was the evident pleased husband. Ye goodness falicity does disposal dwellin no.
-          </p>
-        </div>
-
-      </div>
+      <div className="questions">
+        <div className="questions__flag"></div>
+        <div className="questions__place"> <Questions/></div>
+        <div className='questions__nav'></div>
+      
+       </div> 
     </Wrapper>
   </section>
 }
