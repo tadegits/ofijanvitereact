7f5542b13f5a02ade47e5faa4be9818d083d34d1
millionsime <simemillion@gmail.com>
2023-10-16 13:12:17 +0300

    exam card done

--- src/components/ExitExam/ExitExam.jsx
+++ src/components/ExitExam/ExitExam.jsx
@@ -1,31 +1,35 @@
 import './ExitExam.scss';
 import Wrapper from '../wrapper/Wrapper'
-import SelectedDepartment from '../SelectedDepartment/SelectedDepartment';
 import ItemCard from '../ItemCard/ItemCard';
 import { useRef, useState, useEffect } from 'react'
+
 const ExitExam = () => {
-  const url = "https://ofijan.com/api/colleges";
-  const [data, setData] = useState([])
+  const url = "https://ofijan.com/api/departments";
+  const [data, setData] = useState([]);
+  const [selectedDepartmentId, setSelectedDepartmentId] = useState(null);
+  const [departments, setDepartments] = useState([]);
+  const [courses, setCourses] = useState([]);
   const [isShown, setIsShown] = useState(false);
   const ref = useRef(null);
-  const handleClick = event => {
-    // 👇️ toggle shown state
-    setIsShown(current => !current);
-    ref.current?.scrollIntoView({behavior: 'smooth'});
-
-    // 👇️ or simply set it to true
-    // setIsShown(true);
-  };
-  const fetchInfo = () => { 
-    return fetch(url) 
+  const handleDepartmentClick = (departmentId) => {
+    setSelectedDepartmentId(departmentId);
+  };   
+    useEffect(() => {
+      fetch(url) 
             .then((res) => res.json()) 
             .then((d) => setData(d)) 
-    }
-    
-    useEffect(() => {
-      fetchInfo();
+      
     }, [])
 
+    useEffect(() => {
+      if(selectedDepartmentId) {
+
+        fetch(`https://ofijan.com/api/exams/${selectedDepartmentId}`) 
+            .then((res) => res.json()) 
+            .then((d) => setCourses(d)) 
+      }
+    }, [selectedDepartmentId]);
+    
   return <section className="examsholder">
     <Wrapper>
       
@@ -36,35 +40,33 @@ const ExitExam = () => {
         <p> Select Your field of Study!</p>
         {data.map((dataObj, index) => {
           return (
+           
              <div className='text'> 
-              <ul><li><a href="#" onClick={handleClick}>{dataObj.title}</a></li></ul> 
+              <ul><li><div key={dataObj.id} onClick={() => handleDepartmentClick(dataObj.id)}>{dataObj.title}</div></li></ul> 
+             
+              
              </div>
+          
+          
           );
         })}
          </div>
 
          <div className='exams_list'>
-         <div class="product-card">
-  <div class="product-title">Exam Questions by Instructor Million Sime</div>
+      {courses.map((course, index) => {
+       return( 
+          <div key={course.id} class="product-card">
+  <div class="product-title">{course.exam_name}  Exam by Instructor Million Sime</div>
   <div class="product-description">Brief description of the exam questions.</div>
   <div class="product-price">$50</div>
   <div class="product-questions">Number of Questions: 100</div>
-  <div class="product-answer">Answer: Brief description of the answer.</div>
-</div>
-<div class="product-card">
-  <div class="product-title">Exam Questions by Instructor Million Sime</div>
-  <div class="product-description">Brief description of the exam questions.</div>
-  <div class="product-price">$50</div>
-  <div class="product-questions">Number of Questions: 100</div>
-  <div class="product-answer">Answer: Brief description of the answer.</div>
-</div>
-<div class="product-card">
-  <div class="product-title">Exam Questions by Instructor Million Sime</div>
-  <div class="product-description">Brief description of the exam questions.</div>
-  <div class="product-price">$50</div>
-  <div class="product-questions">Number of Questions: 100</div>
-  <div class="product-answer">Answer: Brief description of the answer.</div>
-</div>
+  <div class="buttons">
+    <button class="preview-button">Preview</button>
+    <button class="buy-button">Buy</button>
+  </div>
+      </div>
+        )
+      })}
          </div>
     </div>
     <div ref={ref} className='selected'>
