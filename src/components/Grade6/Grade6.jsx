925217956dcd3e9852f775d4b6b2d6a9889e0c77
Abduselam-Ali <abduselamali12@gmail.com>
2023-10-16 14:28:56 +0300

    resolved

--- src/components/Grade6/Grade6.jsx
+++ src/components/Grade6/Grade6.jsx
@@ -1,12 +1,6 @@
 import './Grade6.scss'
 import Wrapper from '../wrapper/Wrapper'
-import Questions from '../Questions/Questions'
-import Img1 from '../../assets/resource1.png'
-import Img2 from '../../assets/resource2.png'
-import Img3 from '../../assets/resource3.png'
-import Img4 from '../../assets/resource4.png'
-import Img5 from '../../assets/resource5.png'
-import Img6 from '../../assets/resource6.png'
+
 
 const Grade8 = () => {
   return <section className="features">
