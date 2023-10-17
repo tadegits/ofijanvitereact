fc9361f39c2729f36e9a69f19d3c82587748f5a4
Abduselam-Ali <abduselamali12@gmail.com>
2023-10-17 16:13:34 +0300

    preparing question page

---
+++ src/components/ExamPage/QuestionList.jsx
@@ -0,0 +1,17 @@
+import Img3 from '../../assets/resource3.png'
+
+function QuestionList() {
+  return (
+    <div className="features__feature dark-feature">
+    <img src={Img3} width={200} alt="" />
+    <h4 className="dark-feature-title">
+      Crypto Support
+    </h4>
+    <p>
+      This page display the list of question with question number
+    </p>
+  </div>
+  )
+}
+
+export default QuestionList
---
+++ src/components/ExamPage/QuestionPage.jsx
@@ -0,0 +1,16 @@
+import Img2 from '../../assets/resource2.png'
+function QuestionPage() {
+  return (
+    <div className="features__feature white-feature">
+    <img src={Img2} width={200} alt="" />
+    <h4 className="white-feature-title">
+      Fast and easy registration
+    </h4>
+    <p>
+      Engaged was the evident pleased husband. Ye goodness falicity does disposal dwellin no.
+    </p>
+  </div>
+  )
+}
+
+export default QuestionPage
