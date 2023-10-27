c675e83eca5fc9d01b8cbe33076b31b992551848
millionsime <simemillion@gmail.com>
2023-10-27 14:40:43 +0300

    Root url configured

--- src/Seller/pages/topic/AddTopic.jsx
+++ src/Seller/pages/topic/AddTopic.jsx
@@ -2,30 +2,46 @@ import React, { useState, useEffect } from 'react';
 import PriorityHighIcon from '@mui/icons-material/PriorityHigh';
 import "./topic.scss";
 import Wrapper from '../../../components/wrapper/Wrapper'
+import axios from 'axios';
+import Topic from './Topic';
 export default function AddTopic() {
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
     const [subtopic, setSubTopic] = useState('');
-    const [reference, setReference] = useState('');
-    const [dept_id, setDeptId] = useState('');
+    const [reference_id, setReference] = useState(1);
+    const [department_id, setDeptId] = useState('');
+    const [referenceData, setReferenceData] = useState('');
+    const [user_id, setUserID] = useState('');
     useEffect(() => {
         const loggedInUser = localStorage.getItem("user");
         if (loggedInUser) {
             const userDept = JSON.parse(loggedInUser);
             setDeptId(parseInt(userDept.user.dept_id));
+            const users = JSON.parse(loggedInUser);
+            setUserID(users.user.id);
+            const uri = `http://127.0.0.1:8000/api/all_references/${user_id}`;
+            console.log(uri)
+            axios.get(uri)
+                .then(response => {
+                    setReferenceData(response.data.references);
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                });
 
         }
-    }, []);
+    }, [user_id]);
     const handleSubmit = async () => {
         try {
-            const response = await fetch('https://api.example.com/books', {
+            const response = await fetch('http://127.0.0.1:8000/api/add_topics', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ title, description, subtopic, reference}),
-            });
+                body: JSON.stringify({ title, subtopic, description, reference_id }),
 
+            });
+            console.log(JSON.stringify({ title, subtopic, description, reference_id, department_id, user_id }))
             if (response.ok) {
                 console.log('Topic data sent successfully!');
                 // Reset the form fields
@@ -35,6 +51,7 @@ export default function AddTopic() {
                 setReference('');
             } else {
                 console.log('Failed to send topic data.');
+                console.log()
             }
         } catch (error) {
             console.error('Error:', error);
@@ -88,14 +105,19 @@ export default function AddTopic() {
                                     </div>
                                     <div className="department">
                                         <label>Mention Reference</label>
-                                        <select name='type'
-                                            className='dept'
-                                            value={reference}
+                                        <select
+                                            name="type"
+                                            className="dept"
+                                            value={reference_id}
                                             onChange={(e) => setReference(e.target.value)}
-                                            required>
-                                            <option value="Book">a Book by walter white</option>
-                                            <option value="Journal">Journal</option>
-                                            <option value="Website">Web-link</option>
+                                            required
+                                        >
+                                            {referenceData &&
+                                                referenceData.map((rData) => (
+                                                    <option key={rData.id} value={rData.id}>
+                                                        {rData.title}
+                                                    </option>
+                                                ))}
                                         </select>
                                     </div>
                                 </div>
@@ -105,6 +127,7 @@ export default function AddTopic() {
                             </div>
                         </div>
                     </div>
+                    <Topic />
                 </Wrapper>
             </section>
         </div>
