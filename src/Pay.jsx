2e277ac88b29aea978f38d89ee003f55b84b151f
millionsime <simemillion@gmail.com>
2023-11-07 16:55:00 +0300

    issun

--- src/Pay.jsx
+++ src/Pay.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 function Pay(fname,lname,email,amount, public_key,tx_ref, title) {
- 
+    const generateRandomString = () => {
+        const length = 20;
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        let result = '';
+        for (let i = 0; i < length; i++) {
+          result += characters.charAt(Math.floor(Math.random() * characters.length));
+        }
+        return result;
+      };
+       const [randomString, setRandomString] = useState('');
+       useEffect(() => {
+    const newRandomString = generateRandomString();
+    setRandomString(newRandomString);
+  }, []);
     return (
         <div>
 
 
              <form method="POST" action="https://api.chapa.co/v1/hosted/pay" >
                 <input type="hidden" name="public_key" value="CHAPUBK_TEST-awyvtaEfHkG3crEKM4uLlCwX2vP7ytnK" />
-                <input type="hidden" name="tx_ref" value="weygudemelassaasamedfgh" />
+                <input type="hidden" name="tx_ref" value={randomString} />
                 <input type="hidden" name="amount" value={fname.amount} />
                 <input type="hidden" name="currency" value="ETB" />
                 <input type="hidden" name="email" value={email} />
