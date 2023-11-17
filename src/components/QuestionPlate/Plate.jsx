98fbae59488a90c386ccc1c6d84529f5694b00ab
millionsime <simemillion@gmail.com>
2023-11-17 16:51:00 +0300

    push them all

--- src/components/QuestionPlate/Plate.jsx
+++ src/components/QuestionPlate/Plate.jsx
@@ -3,15 +3,19 @@ import { useParams } from 'react-router-dom';
 import useLoggedInUser from '../../Globals/useLoggedInUser';
 import './plate.scss';
 import Logo from "../../assets/logo.png"
-
+import axios from 'axios';
+import API_BASE_URL from '../../Globals/apiConfig';
+import Swal from 'sweetalert2';
 const Plate = () => {
     const { ofin_id } = useParams();
     const { deptId, userId } = useLoggedInUser();
+    const [answered, setAnswered] = useState(false);
     const [questionData, setQuestionData] = useState([]);
     const [selectedQuestionIndex, setSelectedQuestionIndex] = useState(0);
     const [selectedOptionIndex, setSelectedOptionIndex] = useState(null);
     const [correctAnswersCounter, setCorrectAnswersCounter] = useState(0);
-
+    const [correctAnswer, setCorrectAnswer] = useState('')
+    const alphabet = ["A", "B", "C", "D"];
     useEffect(() => {
         fetch(`https://ofijan.com/api/way_questions/${ofin_id}`)
             .then((res) => res.json())
@@ -29,6 +33,7 @@ const Plate = () => {
     }, [selectedQuestionIndex, questionData]);
     const handleQuestionClick = (index) => {
         setSelectedQuestionIndex(index);
+      
         setSelectedOptionIndex(questionData[index].options.findIndex((option) => option.selected));
     };
     const handleOptionClick = (index) => {
@@ -39,33 +44,28 @@ const Plate = () => {
         }));
         setQuestionData(updatedQuestionData);
         setSelectedOptionIndex(index);
+        //set the correct answer
+setAnswered(true)
+
         if (questionData[selectedQuestionIndex].options[index].correct === '1') {
             setCorrectAnswersCounter((prevCounter) => prevCounter + 1);
         } else {
             setCorrectAnswersCounter((prevCounter) => prevCounter - 1);
-
         }
-
-        // // Send the selected answer to the backend
-        // const selectedQuestion = questionData[questionIndex];
-        // const selectedOption = selectedQuestion.options[optionIndex];
-
-        // axios.post('/selected-answers', {
-        //   user_id: userId,
-        //   question_id: selectedQuestion.id, // Assuming you have question IDs in your data
-        //   option_id: selectedOption.id, // Assuming you have option IDs in your data
-        // })
-        // .then(response => {
-        //   // Handle the response if needed
-        // })
-        // .catch(error => {
-        //   // Handle errors if needed
-        // });
-
-
+        const selectedQuestion = questionData[selectedQuestionIndex];
+        const selectedOptionID = selectedQuestion.options[selectedOptionIndex];
+        axios.post('http://127.0.0.1:8000/api/selected-answers', {
+            user_id: 1,
+            question_id: selectedQuestion,
+            option_id: selectedOptionID,
+        })
+            .then(response => {
+            })
+            .catch(error => {
+            });
     };
     const handleNextClick = () => {
-        if (selectedQuestionIndex < questionData.length - 1) {
+        if (selectedQuestionIndex < 4) {
             setSelectedQuestionIndex((prevIndex) => prevIndex + 1);
             setSelectedOptionIndex(null);
         }
@@ -76,34 +76,45 @@ const Plate = () => {
             setSelectedOptionIndex(null);
         }
     };
+    const handleSweetAlert = (id) => {
+        Swal.fire({
+            text: 'Login to get the rest of the questions!',
+            icon: 'alert',
+            showCancelButton: true,
+            confirmButtonText: 'Login Now',
+            //cancelButtonText: 'Later',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                window.location.href = `/login`;
+            }
+        });
+    };
     return (
         <div className='ofijan_exam_plate'>
-            <div  className='basicInfoPlate'>
+            <div className='basicInfoPlate'>
                 <table>
                     <tr>
                         <th>Picture</th>
                         <th>Basic Information</th>
                     </tr>
                     <tr>
-                        <td><img src={Logo} width={10} height={10} alt='profile picture'/></td>
-                        <td  className='basicInfoPlate2'>
+                        <td><img src={Logo} width={10} height={10} alt='profile picture' /></td>
+                        <td className='basicInfoPlate2'>
                             <tr>
-                            <td>Full Name Million Sime</td>
-                            <td>Institution:  Ofin University</td>
+                                <td>Full Name Million Sime</td>
+                                <td>Institution:  Ofin University</td>
                             </tr>
                             <tr><td>Enrolment Type: Regular</td>
-                            <td>University ID O/UR28147/11</td>
+                                <td>University ID O/UR28147/11</td>
                             </tr>
                             <tr><td>Exam Center: Ofin University</td>
-                            <td>Department: Computer Science</td>
+                                <td>Department: Computer Science</td>
                             </tr>
                         </td>
                     </tr>
                 </table>
-               
             </div>
             <div className='plate'>
-
                 <div className='flag_plate'>
                     <h5>Question {selectedQuestionIndex + 1}</h5>
                     <p>Answer saved</p>
@@ -112,65 +123,91 @@ const Plate = () => {
                     <p><a href='#'>Flag Question</a></p>
                 </div>
                 <div className='question_plate'>
-                    <div className="timePlate"> 
-                    <div className="timebox1"></div>
-                    <div className="timebox2">Time left 20 min</div>
+                    <div className="timePlate">
+                        <div className="timebox1"></div>
+                        <div className="timebox2">Time left 20 min</div>
                     </div>
                     <div className="questionplate">
-                    {questionData.length > 0 && (
-                        <>
-                          <div className="questionText"> 
-                          <p dangerouslySetInnerHTML={{ __html: questionData[selectedQuestionIndex].question_text }} />
-                          </div> 
-                          <div className="choicePlate">
-                            {questionData[selectedQuestionIndex].options.map((option, index) => (
-                                <label
-                                    className={`option_box ${selectedOptionIndex === index ? 'selected' : ''}`}
-                                    key={index}
-                                    onClick={() => handleOptionClick(index)}
-                                >
-                                    <input
-                                        type='radio'
-                                        name={`option_${selectedQuestionIndex}`}
-                                        value={option.option}
-                                        checked={selectedOptionIndex === index}
-                                        readOnly
-                                    />
-                                    {option.option}
-                                </label>
-                                
-                            ))}</div>
-                            <div className='navigation_buttons'>
-                                <button onClick={handlePreviousClick} disabled={selectedQuestionIndex === 0}>
-                                    Previous
-                                </button>
-                                <button onClick={handleNextClick} disabled={selectedQuestionIndex === questionData.length - 1}>
-                                    Next
-                                </button>
-                            </div>
-                        </>
-                    )}
+                        {questionData.length > 0 && (
+                            <>
+                                <div className="questionText">
+                                    <p dangerouslySetInnerHTML={{ __html: questionData[selectedQuestionIndex].question_text }} />
+                                </div>
+                                <div className="choicePlate">
+                                    {questionData[selectedQuestionIndex].options.map((option, index) => {
+                                        const isSelected = selectedOptionIndex === index;
+                                        const isCorrectAnswer = option.correct === '1';
+
+                                        return (
+                                            <label
+                                                className={`option_box ${isSelected ? 'selected' : ''} ${selectedOptionIndex !== null && isCorrectAnswer ? 'correct-answer' : ''}`}
+                                                key={index}
+                                                onClick={() => handleOptionClick(index)}
+                                            >
+                                                <input
+                                                    type='radio'
+                                                    name={`option_${selectedQuestionIndex}`}
+                                                    value={option.option}
+                                                    checked={isSelected}
+                                                    readOnly
+                                                />
+                                                <span className="alphabet">{alphabet[index]}. </span>
+                                                
+                                                {option.option}
+                                                <span className={`correct_is ${isCorrectAnswer ? 'hi': '' } ${selectedQuestionIndex === index ? 'selected' : ''} `} >{alphabet[index]}. </span>
+                                            </label>
+                                        );
+                                    })}
+                                    <h5>Clear Choice</h5>
+                                </div>
+
+                            </>
+                        )}
+                    </div>  <div className='navigation_buttons'>
+                        <button onClick={handlePreviousClick} disabled={selectedQuestionIndex === 0}>
+                            Previous
+                        </button>
+                        <button onClick={handleNextClick} disabled={selectedQuestionIndex === 4}>
+                            Next
+                        </button>
                     </div>
                 </div>
                 <div className='answer_plate'>
                     <h5>Exam Navigation</h5>
                     <div className='answer_plate'>
-                    {questionData.map((question, index) => (
-                        <div className={`answer_box_holder ${selectedQuestionIndex === index ? 'selected' : ''} ${question.options.some(option => option.selected) ? 'answered' : ''}`}
-                            key={index}
-                            onClick={() => handleQuestionClick(index)}>
-                            <div className='answer_box'>{index + 1}</div>
-                            <div
-                                className={`answer_box ${selectedQuestionIndex === index ? 'selected' : ''} ${question.options.some(option => option.selected) ? 'answered' : ''}`}
-                                key={index}
-                                onClick={() => handleQuestionClick(index)}>
-                            </div>
+                        {questionData.map((question, index) => (
+                            <>
+                                {index < 5 ? (<>
+                                    <div
+                                        className={`answer_box_holder ${selectedQuestionIndex === index ? 'selected' : ''} ${question.options.some(option => option.selected) ? 'answered' : ''}`}
+                                        key={index}
+                                        onClick={() => handleQuestionClick(index)}>
+                                        <div className='answer_box'>{index + 1}</div>
+                                        <div
+                                            className={`answer_box ${selectedQuestionIndex === index ? 'selected' : ''} ${question.options.some(option => option.selected) ? 'answered' : ''}`}
+                                            key={index}
+                                            onClick={() => handleQuestionClick(index)}
+                                            style={{ pointerEvents: index < 5 ? 'auto' : 'none' }}>
+                                        </div>
+                                    </div>
+                                </>) : (<>
+                                    <div
+                                        className={`answer_box_holder ${selectedQuestionIndex === index ? 'selected' : ''} ${question.options.some(option => option.selected) ? 'answered' : ''}`}
+                                        key={index} onClick={() => handleSweetAlert(index)}>
+                                        <div className='answer_box'>{index + 1}</div>
+                                        <div
+                                            className={`answer_box ${selectedQuestionIndex === index ? 'selected' : ''} ${question.options.some(option => option.selected) ? 'answered' : ''}`}
+                                            key={index}
 
-                        </div>
-                    ))}
+                                            style={{ pointerEvents: index < 5 ? 'auto' : 'none' }}>
+                                        </div>
+                                    </div>
+                                </>)}
+                            </>
+                        ))}
                     </div>
                 </div>
-                
+
             </div>
             <div className='correct_counter'>Correct Answers: {correctAnswersCounter}</div>
         </div>
@@ -178,4 +215,3 @@ const Plate = () => {
 };
 
 export default Plate;
-
