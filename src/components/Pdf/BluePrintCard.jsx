e200d64ee30babb113b5766b8cf7e9c67cef897d
millionsime <simemillion@gmail.com>
2024-02-16 17:46:55 +0300

    push

--- src/components/Pdf/BluePrintCard.jsx
+++ src/components/Pdf/BluePrintCard.jsx
@@ -1,41 +1,46 @@
-import React, { useState, useEffect } from 'react'
-import { Card, Avatar, Descriptions,  Collapse} from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Card } from 'antd';
+import { Link } from 'react-router-dom';
 import './pdf.scss';
 import API_BASE_URL from '../../Globals/apiConfig';
-const { Panel } = Collapse;
+
 const BluePrintCard = () => {
   const [bluePrintData, setBluePrintData] = useState([]);
-  useEffect(() => {
-        const url = `${API_BASE_URL}/fetch_blue_print_path`;
-        fetch(url)
-            .then(response => response.arrayBuffer())
-            .then(arrayBuffer => {
-                setBluePrintData(new Uint8Array(arrayBuffer));
-                setLoading(false); 
-            })
-            .catch(error => {
-                console.error('Error fetching PDF:', error);
-            });
-}, []);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const fetchBluePrint = async () => {
+      try {
+        const response1 = await fetch(`${API_BASE_URL}/fetch_blue_print_path`);
+        const data1 = await response1.json();
+        setBluePrintData(data1);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchBluePrint();
+  }, []);
+
+  console.log('blueprint', bluePrintData);
   return (
     <div>
-    <Card title={<h2>Blue Print</h2>}>
-{bluePrintData.length? 
-
-(bluePrintData.map((bp, index)=>{
-  <>
-  {bp}
-  </>}
-  ))
-  :
-  (<></>)
-}
-     
-    </Card>
-
+      <Card title={<h2>Blue Print</h2>}>
+        {loading ? (
+          <p>Loading...</p>
+        ) : bluePrintData.length ? (
+          bluePrintData.map((bp, index) => (
+            <Link to={`/display-pdf/${bp}`} state={{ pdfs: bp }}>
+              <p key={index}>{bp}</p>
+            </Link> 
+          ))
+        ) : (
+          <p>No blueprint data available</p>
+        )}
+      </Card>
     </div>
-  )
-}
-
-export default BluePrintCard
+  );
+};
+export default BluePrintCard;
