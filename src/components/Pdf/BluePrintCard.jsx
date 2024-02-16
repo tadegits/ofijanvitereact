7539e7fd055bba761b34f6ccd2c808e6a4069dfa
millionsime <simemillion@gmail.com>
2024-02-16 17:28:59 +0300

    done

--- src/components/Pdf/BluePrintCard.jsx
+++ src/components/Pdf/BluePrintCard.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Card, Avatar, Descriptions,  Collapse} from 'antd';
 import './pdf.scss';
+import API_BASE_URL from '../../Globals/apiConfig';
 const { Panel } = Collapse;
 const BluePrintCard = () => {
+  const [bluePrintData, setBluePrintData] = useState([]);
+  useEffect(() => {
+        const url = `${API_BASE_URL}/fetch_blue_print_path`;
+        fetch(url)
+            .then(response => response.arrayBuffer())
+            .then(arrayBuffer => {
+                setBluePrintData(new Uint8Array(arrayBuffer));
+                setLoading(false); 
+            })
+            .catch(error => {
+                console.error('Error fetching PDF:', error);
+            });
+}, []);
 
   return (
     <div>
     <Card title={<h2>Blue Print</h2>}>
+{bluePrintData.length? 
 
+(bluePrintData.map((bp, index)=>{
+  <>
+  {bp}
+  </>}
+  ))
+  :
+  (<></>)
+}
      
     </Card>
 
--- src/components/Pdf/DisplayPdf.jsx
+++ src/components/Pdf/DisplayPdf.jsx
@@ -46,13 +46,8 @@ const DisplayPdf = ({ onClose, formData, studentName }) => {
     if (depts) {
         return  <Wrapper className='pdfs'>
         <div className="display-pdf-container">
-        
-                
             <Row gutter={24}>
-                
                     <Col xs={24} sm={24} md={24} lg={5} xl={5}>
-                        {/**/}
-                        
                         <CollegeDepartment onSelectDepartment={setSelectedDepartment} /> 
                     </Col>
                     <Col xs={24} sm={24} md={24} lg={14} xl={14} className='pdf__viewer'>
@@ -64,7 +59,6 @@ const DisplayPdf = ({ onClose, formData, studentName }) => {
                          <BluePrintCard /> 
                     </Col>
                 </Row>
-            
             </div>
         </Wrapper>;
     }
