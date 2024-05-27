b9c8a270b510e813d1d9b82049ae4988ce8b62dc
tadegits <given.gedlu@gmail.com>
2024-05-27 18:11:09 +0300

    som4 changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,15 @@ import App from './App.jsx'
 import './index.scss'
 import { Provider } from 'react-redux';
 import { store } from './app/store.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId='608370973698-1hih3bj9qnsr0rd2pjiv2pji1g5jo7cu.apps.googleusercontent.com'>
       <Provider store={store}>
-          <BrowserRouter>
+          <HashRouter>
             <App />
-          </BrowserRouter>
+          </HashRouter>
       </Provider>
     </GoogleOAuthProvider>
   </React.StrictMode>
