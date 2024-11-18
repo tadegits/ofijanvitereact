0a3e3ff9c790dbd8d7ca2e3386ae162c4df580fd
millionsime <simemillion@gmail.com>
2024-11-18 13:01:43 +0300

    lale

--- src/components/Pdf/BluePrint.jsx
+++ src/components/Pdf/BluePrint.jsx
@@ -17,15 +17,8 @@ const BluePrint = () => {
 <Wrapper className='pdfs'>
     <div className="display-pdf-container">
     <Row gutter={24} className=''>
-    <Col xs={24} sm={24} md={24} lg={5} xl={5}>
-      <Card title={<h2>2015 Exit Exam</h2>}>
-        <Link to='../2015_exit_pdfs'>
-          <Button >Open</Button>
-        </Link>
-      </Card>
-    </Col>
-    <Col xs={24} sm={24} md={24} lg={14} xl={14} className='display-pdf-container'>
-      <h1> Ethiopian National Exit Exam Blue Print</h1>
+    
+    <Col xs={24} sm={24} md={24} lg={14} xl={14} className=''>
      <BluePrintCard/>
     </Col>
     <Col xs={24} sm={24} md={24} lg={5} xl={5}>
